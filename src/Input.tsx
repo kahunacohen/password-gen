20562656e87564116a27a12bf3d5e697739cbd26
Aaron Cohen <aaroncohendev@gmail.com>
2023-05-04 13:35:43 +0300

    checkpoint

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,4 +1,18 @@
 export function Input(props: { length: number; setPasswordLength: Function }) {
+  function handleInputTicks(evt: any) {
+    switch (evt.target.dataset.passParams) {
+      case "uppercase":
+        break;
+      case "lowercase":
+        break;
+      case "numbers":
+        break;
+      case "specialchars":
+        break;
+      default:
+        console.error("an invalid password input parameter was chosen");
+    }
+  }
   return (
     <div className="Input">
       <label className="length-label" htmlFor="length-input">
@@ -11,22 +25,22 @@ export function Input(props: { length: number; setPasswordLength: Function }) {
         max="16"
         defaultValue={props.length}
       ></input>
-      <div className="checkboxes-ct">
+      <div className="checkboxes-ct" onChange={handleInputTicks}>
         <div className="checkbox-ct">
           <label>Uppercase</label>
-          <input type="checkbox"></input>
+          <input data-pass-param="uppercase" type="checkbox"></input>
         </div>
         <div className="checkbox-ct">
           <label>Lowercase</label>
-          <input type="checkbox"></input>
+          <input data-pass-param="lowercae" type="checkbox"></input>
         </div>
         <div className="checkbox-ct">
           <label>Numbers</label>
-          <input type="checkbox"></input>
+          <input data-pass-param="numbers" type="checkbox"></input>
         </div>
         <div className="checkbox-ct">
           <label>Special Characters</label>
-          <input type="checkbox"></input>
+          <input data-pass-param="specialchars" type="checkbox"></input>
         </div>
       </div>
     </div>
